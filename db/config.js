f42f216b39d0652122fe1510ef798505c798ace7
Gordon Preumont <preumontgr@gmail.com>
2020-06-22 15:41:36 +0200

    setup mysql and pug

---
+++ db/config.js
@@ -0,0 +1,11 @@
+const  mysql = require('mysql');
+
+const  connection =    mysql.createConnection({
+
+    host: 'localhost', 
+ 	user: 'root', 
+ 	password: '604604gg',
+ 	database: 'mvc',
+});
+
+module.exports = connection;
--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ const router = require('../routes');
 
 app.use('/', router);
 
+app.set('views', './views');
+app.set('view engine', 'pug');
+
 app.listen(3000, () => {
  console.log('Example app listening on port 3000!')
 });
